9dbb06f27fb2c2c7b87f1299bfebc3092de7b910
takapon564 <ku.takahito058@gmail.com>
2019-01-14 10:40:32 +0900

    edit,updateアクションの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,13 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to posts_url, notice: "記事「#{post.title}]」を更新しました。"
   end
 
   private 
