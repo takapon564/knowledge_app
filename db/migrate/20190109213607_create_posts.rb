e6049c7dc4f41c9a7c00007f5774f39fad2c3bf6
takapon564 <ku.takahito058@gmail.com>
2019-01-14 11:48:50 +0900

    マイグレーションの実行

--- db/migrate/20190109213607_create_posts.rb
+++ db/migrate/20190109213607_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :title
+      t.string :title, limit: 30, null: false
       t.text :content
 
       t.timestamps
