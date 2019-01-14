40a6d7fdb294902c0beadf6294ced3fdf25b24c2
takapon564 <ku.takahito058@gmail.com>
2019-01-14 11:36:51 +0900

    マイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_213607) do
+ActiveRecord::Schema.define(version: 2019_01_14_022751) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
