2d42cc57a563539801c64739f7b20535ac1528bf
takapon564 <ku.takahito058@gmail.com>
2019-01-17 05:41:27 +0900

    usersコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+    get 'users/edit'
+    get 'users/show'
+    get 'users/index'
+  end
   root to: 'posts#index'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
