73ab676f905c6c5a72c527ceb81fb207b420f143
takapon564 <ku.takahito058@gmail.com>
2019-01-17 06:14:57 +0900

    アクションの実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,14 +12,33 @@ class Admin::UsersController < ApplicationController
       render :new
     end
   end
+  
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー『#{@user.name}』を更新しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー『#{@user.name}』を削除しました。"
+  end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def index
+    @users = User.all
   end
 
   private
