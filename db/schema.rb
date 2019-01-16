5a021981658daa5ecbd5bc1360fd89ea5ac70c64
takapon564 <ku.takahito058@gmail.com>
2019-01-17 05:37:02 +0900

    マイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_200831) do
+ActiveRecord::Schema.define(version: 2019_01_16_203312) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_01_16_200831) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
