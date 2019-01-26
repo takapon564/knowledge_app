35fc44ccd0cdfcff4f21a1aab641615fec686752
takapon564 <ku.takahito058@gmail.com>
2019-01-26 19:29:34 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root to:  'lists#index'
   get '/show_profile', to: 'lists#show_profile'
-  resources :lists
+  resources :lists 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   namespace :admin do
     resources :users
   end
-  resources :posts
+  resources :posts 
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
