a5793bf3c0d0718303190f6f9506dea50f525c67
takapon564 <ku.takahito058@gmail.com>
2019-01-28 05:38:27 +0900

    バグの修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     resources :users
   end
   resources :posts do
-    post :confirm, action: confirm_new, on: :new
+    post :confirm, action: :confirm_new, on: :new
   end
     
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
